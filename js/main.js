16a655e31bfc042505a1aa14864ba48a7bf4dd07
In0lin <137963896+In0lin@users.noreply.github.com>
2023-06-28 03:16:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper( ".swiper", {
+    effect: "fade",
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    aytoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    }
+    
+    },
+ );
